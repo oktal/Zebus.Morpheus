c1be58e697965275bc536661b931725bfb041584
Mathieu Stefani <auquetal@gmail.com>
2024-04-03 19:05:04 +0200

    feat(simulation): add a BasicEvent simulation

---
+++ Simulation/BasicEvent/Messages.cs
@@ -0,0 +1,11 @@
+using Abc.Zebus;
+using ProtoBuf;
+
+namespace Zebus.Morpheus.Simulation.BasicEvent;
+
+[ProtoContract]
+public class BasicEvent : IEvent
+{
+	[ProtoMember(1)]
+	public int Seq { get; set; }
+}
